7083cbce487749ad194b237816a15ff1dc9fd432
Bhargavnaidu2808 <Bhargav12mb@gmail.com>
2026-07-23 10:39:21 +0530

    Switch from yum to dnf and update Java version
    Updated package manager commands from yum to dnf and changed Java version from 17 to 21.

--- jenkins (1).sh
+++ jenkins (1).sh
@@ -1,11 +1,17 @@
-sudo yum update –y
+sudo dnf update -y
+
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade
-sudo yum install java-17-amazon-corretto -y
-sudo yum install jenkins git -y
+
+sudo dnf install java-21-amazon-corretto -y
+
+sudo dnf install git jenkins -y
+
 sudo systemctl enable jenkins
+
 sudo systemctl start jenkins
+
 sudo systemctl status jenkins
 sudo mkdir -p /var/tmp_disk
 sudo chmod 1777 /var/tmp_disk
